175f32a393ace6eca2b8b15abac1e2e9252aecb3
Enno Boland <g@s01.de>
2014-06-09 15:55:10 +0200

    add some documentation to menu.sh

--- menu.sh
+++ menu.sh
@@ -1,17 +1,41 @@
+# MENU
+# ====
+# Menu is a wrapper around `dialog` to ease the usage of complex menu systems
+# it is written in in Posix compliant shell script.
+# It consists of the following commands:
+#
+# menu_add MENU ITEM_NAME DESCRIPTION
+#  this command adds a new definition to the menu named `MENU`
+#  the item is called `ITEM_NAME` and its description is `DESCRIPTION`
+#
+# menu MENU TITLE ...
+#  this command will call `dialog` with all items you've added to this menu
+#  with the `menu_add` command.
+#  When the user selects an item, `menu` will call a function named
+#  `<MENU>_<ITEM_NAME>` and add the trailing arguments of menu to the function
+#  call.
+#  If the user cancels the dialog, <MENU>_cancel will be called
+
+
 MENU_ENTRIES=
+CURRENT_ITEMS=
 
 menu() {
 	menu=$1
 	title=$2
+	shift 2
+	current_item=`echo "$CURRENT_ITEMS" | grep "^$menu:" | cut -d: -f2-`
 	result=`echo "$MENU_ENTRIES" | grep "^$menu:" | cut -d: -f2- | \
 		xargs -d '\n' \
-		$DIALOG --default-item "$CURRENT_ITEM" \
+		$DIALOG --default-item "$current_item" \
 			--title "$title" \
 			--menu "$MENU_LABEL" 10 70 0`
+	res=$?
+	CURRENT_ITEMS=`echo "$CURRENT_ITEMS" | grep -v "^$menu:"; echo $menu:$result`
 
-	case $? in
-		0) ${menu}_${result} ;;
-		123) type ${menu}_cancel 2> /dev/null && ${menu}_cancel ;;
+	case $res in
+		0)   ${menu}_${result} $@ ;;
+		123) type ${menu}_cancel 2> /dev/null && ${menu}_cancel $@ ;;
 	esac
 }
 
@@ -20,9 +44,5 @@ menu_add() {
 	name=$2
 	desc=$3
 
-	if [ "$MENU_ENTRIES" ]; then
-		MENU_ENTRIES="$MENU_ENTRIES\n$menu:$name\n$menu:$desc"
-	else
-		MENU_ENTRIES="$menu:$name\n$menu:$desc"
-	fi
+	MENU_ENTRIES="$MENU_ENTRIES\n$menu:$name\n$menu:$desc"
 }
