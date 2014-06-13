97da42d1dcab30b5387a54edc763f3802534c442
Enno Boland <g@s01.de>
2014-06-13 17:10:48 +0200

    02-network-wifi.sh: add dialog to configure wireless networks

--- applets/02-network-wifi.sh
+++ applets/02-network-wifi.sh
@@ -1,4 +1,21 @@
 menu_add NETWORK "wifi" "Set up wireless network"
+
+menu_add WIFI "wep"
+menu_add WIFI "wpa"
+WIFI_select() {
+	encryption=$1
+	device=$2
+
+	result=`$DIALOG --form "$encryption: Wireless configuration for ${device}\n" 0 0 0 \
+		"SSID:" 1 1 "" 1 16 30 0 \
+		"Password:" 3 1 "" 3 16 30 0` || return 1
+}
+
 NETWORK_wifi() {
-	device=`select_nic wireless`
+	set -x
+	while : ; do
+		device=`select_nic wireless` || return 1
+		menu WIFI "$device" && break;
+		sleep 5;
+	done
 }
